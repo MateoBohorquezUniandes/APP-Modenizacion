72e271566b01aec503951eda33826978b6ae4f20
MateoBohorquezUniandes <i.bohorquezp@uniandes.edu.co>
2024-07-28 15:40:22 -0500

    Entrega S8

---
+++ Backend/libros/src/logica/deserializers.py
@@ -0,0 +1,30 @@
+import datetime
+from marshmallow import Schema, fields, validate, ValidationError
+
+class CreateLibroSchema(Schema):
+    titulo = fields.String(
+        required=True,
+        validate=validate.Regexp(r"^[a-zA-Z0-9]*$"),
+        error_messages={"invalid": "Titulo Invalido"},
+    )
+    autor = fields.String(
+        required=True,
+        validate=validate.Regexp(r"^[a-zA-Z0-9]*$"),
+        error_messages={"invalid": "Autor Invalido"},
+    )
+    ISBN = fields.String(required=True)
+    fechaPublicacion = fields.DateTime(
+        error_messages={"invalid": "fecha Invalida"},
+    )
+    paginas = fields.Integer(required = True)
+    editorial = fields.String(
+        required=True,
+        validate=validate.Regexp(r"^[a-zA-Z0-9]*$"),
+        error_messages={"invalid": "Editorial Invalido"},
+    )
+    portada = fields.String(
+        required=True,
+    )
+
+create_schema = CreateLibroSchema()
+
